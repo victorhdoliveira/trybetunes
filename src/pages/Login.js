3b6e0159d0c231092ba81a646fa8608427cba77f
Victor Hugo <victorhdoliveira@gmail.com>
2022-10-03 18:36:25 -0300

    Requisito nº 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,65 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  state = {
+    isDisable: true,
+    user: '',
+  };
+
+  handleChangeUser = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      user: value,
+    }, () => {
+      const { user } = this.state;
+      const minLength = 3;
+      const teste = user.length < minLength;
+      this.setState({ isDisable: teste });
+    });
+  };
+
+  handleClick = () => {
+    const { user } = this.state;
+    const { history } = this.props;
+    createUser({ name: user });
+    history.push('/search');
+  };
+
   render() {
+    const { isDisable, user } = this.state;
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <form>
+          <label htmlFor="login">
+            <input
+              data-testid="login-name-input"
+              value={ user }
+              type="text"
+              onChange={ this.handleChangeUser }
+              placeholder="Insira seu nome"
+            />
+          </label>
+          <button
+            data-testid="login-submit-button"
+            disabled={ isDisable }
+            type="button"
+            onClick={ this.handleClick }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Login;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import Loading from '../components/Loading';
+import getMusics from '../services/musicsAPI';
 
 class Search extends React.Component {
+  state = {
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const fetchMusics = await getMusics();
+    this.setState({
+      loading: false,
+    }, fetchMusics);
+  }
+
   render() {
+    const { loading } = this.state;
     return (
-      <div data-testid="page-search" />
+      <div data-testid="page-search">
+        {
+          loading ? (
+            <Loading />
+          ) : (
+            ''
+          )
+        }
+      </div>
     );
   }
 }
