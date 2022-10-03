ded8874b0270d3992829290615456eee327e5d2b
Victor Hugo <victorhdoliveira@gmail.com>
2022-10-03 15:31:14 -0300

    Requisito nº 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Routes from './Routes';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <Routes />
+    );
   }
 }
 
---
+++ src/Routes.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/search" component={ Search } />
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
+    );
+  }
+}
+
+export default Routes;
